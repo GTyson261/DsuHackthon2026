610944f6e16e8215b46d5bcebcfaf38e51928ad1
Bryant Ferguson <btbf569708@gmail.com>
2026-04-18 17:03:22 -0400

    Finished backend setup

---
+++ backend/src/main/java/com/example/disaster_opportunity_engine/DisasterOpportunityEngineApplication.java
@@ -0,0 +1,13 @@
+package main.java.com.example.disaster_opportunity_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisasterOpportunityEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisasterOpportunityEngineApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/disaster_opportunity_engine/DisasterOpportunityEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.example.disaster_opportunity_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisasterOpportunityEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
